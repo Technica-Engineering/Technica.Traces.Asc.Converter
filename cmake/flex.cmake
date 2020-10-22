0c19105e23e3f734aa7b11ca752e01b9c158b20e
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2020-10-22 18:21:59 +0200

    Use homebrew path

---
+++ cmake/flex.cmake
@@ -0,0 +1,27 @@
+
+if (WIN32)
+    include(FetchContent)
+    FetchContent_Declare(
+        flex
+        URL https://github.com/lexxmark/winflexbison/releases/download/v2.5.23/win_flex_bison-2.5.23.zip
+    )
+    FetchContent_MakeAvailable(flex)
+
+    set(FLEX_FLAGS "--wincompat")
+    set(FLEX_DIR "${flex_SOURCE_DIR}")
+endif()
+
+if (APPLE)
+    # On macOS, search Homebrew for keg-only versions of Bison and Flex. Xcode does
+    # not provide new enough versions for us to use.
+    execute_process(
+        COMMAND brew --prefix flex 
+        RESULT_VARIABLE BREW_FLEX
+        OUTPUT_VARIABLE BREW_FLEX_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    if (BREW_FLEX EQUAL 0 AND EXISTS "${BREW_FLEX_PREFIX}")
+        message(STATUS "Found Flex keg installed by Homebrew at ${BREW_FLEX_PREFIX}")
+        set(FLEX_DIR "${BREW_FLEX_PREFIX}/bin")
+    endif()
+endif()
